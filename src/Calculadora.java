43f6c0631c0135e6112e66b0230b19714ccdfae4
ivanff07 <naviff07@gmail.com>
2025-10-31 13:36:03 +0100

    Cambios

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,14 +1,17 @@
-
-import java.util.Scanner;
 public class Calculadora {
-    public static void main(String[] args) {
-        System.out.println("=== Calculadora Básica ===");
-        int a = 10;
-        int b = 5;
 
-        System.out.println("Suma: " + (a + b));
-        System.out.println("Resta: " + (a - b));
-        System.out.println("Multiplicación: " + (a * b));
-        System.out.println("División: " + (a / b));
-    }
-}
+	public static void main(String[] args) {
+
+
+		        System.out.println("=== Calculadora Básica ===");
+
+		        int a = 10;
+		        int b = 5;
+
+		        System.out.println("Suma: " + (a + b));
+		        System.out.println("Resta: " + (a - b));
+		        System.out.println("Multiplicación: " + (a * b));
+		        System.out.println("División: " + (a / b));
+		        System.out.println("Potencia: " + Math.pow(a, b));
+		    }
+		}
